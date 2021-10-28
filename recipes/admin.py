2daa3d59ffa094b4ba33ba4b34f309a1fcd3b250
AnaStasia1331 <taurus1331@mail.ru>
2021-10-28 18:12:50 +0000

    create models for recipe and course and execute migrations

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Recipe, Course
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'course',
+        'recipe_name',
+        'ingredients',
+        'steps',
+        'preparation_time',
+        'image',
+    )
+
+    ordering = ('recipe_name',)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'course_choice',
+    )
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Course, CourseAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+
+    class Meta:
+        ordering = ['recipe_name']
+
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    recipe_name = models.CharField(max_length=50)
+    ingredients = models.TextField(null=True, blank=True)
+    steps = models.TextField(null=True, blank=True)
+    preparation_time = models.CharField(max_length=5, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.recipe_name
+
+
+class Course(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Courses'
+
+    COURSE_CHOICES = [
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('snacks', 'Snacks'),
+        ('desserts', 'Desserts'),
+        ('others', 'Others'),
+    ]
+
+    course_choice = models.CharField(
+        max_length=15,
+        choices=COURSE_CHOICES,
+    )
+
+    def __str__(self):
+        return self.course_choice

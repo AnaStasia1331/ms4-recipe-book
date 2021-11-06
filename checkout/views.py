ef2d0972aec8ca7321b69d3883160f9c6339b4b8
AnaStasia1331 <taurus1331@mail.ru>
2021-11-06 13:31:10 +0000

    fix the redirection to stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 import stripe
 from django.conf import settings
@@ -9,6 +9,7 @@ from django.conf import settings
 def checkout(request):
     try:
         stripe.api_key = settings.STRIPE_API_KEY
+        WEBSITE_DOMAIN = "http://127.0.0.1:8000"
         checkout_session = stripe.checkout.Session.create(
             customer_email='customer@example.com',
             submit_type='donate',
@@ -19,7 +20,7 @@ def checkout(request):
             line_items=[
                 {
                     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                    'price': 'prod_KXtWCZafaoOBjA',
+                    'price': 'price_1JsnjmKjt9KdFmTFVRRJuuVQ',
                     'quantity': 1,
                 },
             ],
@@ -27,16 +28,18 @@ def checkout(request):
               'card',
             ],
             mode='payment',
-            success_url=reverse('success'),
-            cancel_url=reverse('cancel'),
+            success_url=WEBSITE_DOMAIN + reverse('success'),
+            cancel_url=WEBSITE_DOMAIN + reverse('cancel'),
         )
     except Exception as e:
         return str(e)
 
     return redirect(checkout_session.url, code=303)
 
+
 def success(request):
     return render(request, 'checkout/success.html')
+
 
 def cancel(request):
     return render(request, 'checkout/cancel.html')

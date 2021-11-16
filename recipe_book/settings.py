012ac8d29ac239e532a5b224b202ed6a81719aed
AnaStasia1331 <taurus1331@mail.ru>
2021-11-16 17:58:34 +0000

    attempt to fix emails sending in heroku

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -204,7 +204,7 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if DEBUG:
+if 'DJANGO_DEBUG' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'recipe.book@example.com'
 else:

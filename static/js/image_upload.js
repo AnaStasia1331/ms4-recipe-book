87a9d1fb2f7d31af17221640536989417f4f02a7
AnaStasia1331 <taurus1331@mail.ru>
2021-11-20 11:48:17 +0000

    add Info modal html; add js to trigger the Info modal instead of the alert message; write title and message to the popup depending on validation error

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -5,33 +5,38 @@
 window.addEventListener('DOMContentLoaded', event => {
     // Overwrite the default behavior of bootstrap-filestyle plugin 
     // http://markusslima.github.io/bootstrap-filestyle/
-    $(':file').filestyle({
+    const fileInputField = $(':file');
+    fileInputField.filestyle({
         size: 'lg',
         btnClass: 'btn-warning',
         text: 'Upload',
         'onChange': function (files) {
             const file = files[0];
 
+            // Show Info modal when user tries to upload disallowed file extensions 
             // Similar to the source https://javacodepoint.com/file-upload-validations-in-jquery/
             const fileName = file.name;
-            const extension = fileName.substr(fileName.lastIndexOf("."));
+            const extension = fileName.substr(fileName.lastIndexOf('.'));
             const allowedExtensions =
                 /(\.jpg|\.jpeg|\.png)$/i;
             var isAllowed = allowedExtensions.test(extension);
             if (!isAllowed) {
-                alert("Only files with the following extensions are allowed: jpg, jpeg, png.");
-                $(":file").filestyle('clear');
+                updateAndToggleInfoModal('Invalid file type', 'Only files with the following extensions are allowed: jpg, jpeg, png.');
+                fileInputField.filestyle('clear');
                 return false;
             }
 
+            // Show Info modal when user tries to upload file larger than 4MB 
             // Similar to the source https://www.geeksforgeeks.org/validation-of-file-size-while-uploading-using-javascript-jquery/
             const size = (file.size / 1024 / 1024).toFixed(2);
             if (size > 4) {
-                alert("Maximum upload file size must be 4MB.");
-                $(":file").filestyle('clear');
+                // alert('Maximum upload file size must be 4MB.');
+                updateAndToggleInfoModal('File is too large', 'Maximum upload file size must be 4MB.');
+                fileInputField.filestyle('clear');
                 return false;
             }
 
+            // Show Info modal when user tries to upload images of non-landscape orientation 
             // Similar to the source https://stackoverflow.com/a/41063194/10928662
             var img = new Image();
             img.onload = function () {
@@ -39,13 +44,20 @@ window.addEventListener('DOMContentLoaded', event => {
                 var width = this.width;
                 URL.revokeObjectURL(this.src);
                 if (height > width || width == height) {
-                    alert("To ensure your image display well, we recommended to upload images of ladscape orientation where width is more than height.");
+                    updateAndToggleInfoModal('Recommended image orientation', 'To ensure your image display well, we recommend to upload images of ladscape orientation where width is more than height.');
                     return false;
                 }
                 URL.revokeObjectURL(this.src);
             };
             var objectURL = URL.createObjectURL(file);
             img.src = objectURL;
+
+            // Update the Info modal title and body message and toggle it 
+            function updateAndToggleInfoModal(title, message) {
+                $('.modal-title').text(title);
+                $('.modal-body').text(message);
+                $('#informModal').modal('toggle');
+            }
         }
     });
 });

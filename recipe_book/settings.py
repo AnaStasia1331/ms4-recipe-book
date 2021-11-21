fcac374254a47fad00b58f7b48cc49fbae8f6ade
AnaStasia1331 <taurus1331@mail.ru>
2021-11-21 20:03:53 +0000

    set SECURE_SSL_REDIRECT flag for heroku; refactor image upload functions

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -212,6 +212,8 @@ if 'ON_HEROKU' in os.environ:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    # Redirect http to https
+    SECURE_SSL_REDIRECT = True
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'recipe.book@example.com'
--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -13,51 +13,77 @@ window.addEventListener('DOMContentLoaded', event => {
         'onChange': function (files) {
             const file = files[0];
 
-            // Show Info modal when user tries to upload disallowed file extensions 
-            // Similar to the source https://javacodepoint.com/file-upload-validations-in-jquery/
-            const fileName = file.name;
-            const extension = fileName.substr(fileName.lastIndexOf('.'));
-            const allowedExtensions =
-                /(\.jpg|\.jpeg|\.png)$/i;
-            var isAllowed = allowedExtensions.test(extension);
-            if (!isAllowed) {
-                updateAndToggleInfoModal('Invalid file type', 'Only files with the following extensions are allowed: jpg, jpeg, png.');
-                fileInputField.filestyle('clear');
+            if (isExtensionNotAllowed(file)) {
+                return false;
+            } 
+
+            if (isFileSizeTooLarge(file)) {
                 return false;
             }
 
-            // Show Info modal when user tries to upload file larger than 4MB 
-            // Similar to the source https://www.geeksforgeeks.org/validation-of-file-size-while-uploading-using-javascript-jquery/
-            const size = (file.size / 1024 / 1024).toFixed(2);
-            if (size > 4) {
-                // alert('Maximum upload file size must be 4MB.');
-                updateAndToggleInfoModal('File is too large', 'Maximum upload file size must be 4MB.');
-                fileInputField.filestyle('clear');
-                return false;
-            }
+            // if (!isImgOrientationLandscape(file)) {
+            //     return false;
+            // }
 
-            // Show Info modal when user tries to upload images of non-landscape orientation 
-            // Similar to the source https://stackoverflow.com/a/41063194/10928662
-            var img = new Image();
-            img.onload = function () {
-                var height = this.height;
-                var width = this.width;
-                URL.revokeObjectURL(this.src);
-                if (height > width || width == height) {
-                    updateAndToggleInfoModal('Recommended image orientation', 'To ensure your image display well, we recommend to upload images of ladscape orientation where width is more than height.');
-                    return false;
-                }
-                URL.revokeObjectURL(this.src);
-            };
-            var objectURL = URL.createObjectURL(file);
-            img.src = objectURL;
+             // Show Info modal when user tries to upload images of non-landscape orientation 
+             // Similar to the source https://stackoverflow.com/a/41063194/10928662
+             const img = new Image();
+             img.onload = function () {
+                 const height = this.height;
+                 const width = this.width;
+                 URL.revokeObjectURL(this.src);
+                 if (height > width || width == height) {
+                     updateAndToggleInfoModal('Recommended image orientation',
+                     'To ensure your image display well, we recommend to upload images of ladscape orientation where width is more than height.',
+                     'Okay');
+                     return false;
+                 }
+                 URL.revokeObjectURL(this.src);
+             };
+             var objectURL = URL.createObjectURL(file);
+             img.src = objectURL;
 
-            // Update the Info modal title and body message and toggle it 
-            function updateAndToggleInfoModal(title, message) {
+
+            // Update the Info modal title, body message, button name and toggle it 
+            function updateAndToggleInfoModal(title, message, buttonName) {
                 $('.modal-title').text(title);
                 $('.modal-body').text(message);
+                $('.modal-footer > button').text(buttonName);
                 $('#informModal').modal('toggle');
             }
+
+            function isExtensionNotAllowed(file) {
+                // Show Info modal when user tries to upload disallowed file extensions 
+                // Similar to the source https://javacodepoint.com/file-upload-validations-in-jquery/
+                const fileName = file.name;
+                const extension = fileName.substr(fileName.lastIndexOf('.'));
+                const allowedExtensions =
+                    /(\.jpg|\.jpeg|\.png)$/i;
+                var isAllowed = allowedExtensions.test(extension);
+                if (isAllowed) {
+                    return false;
+                } else {
+                    updateAndToggleInfoModal('Invalid file type', 'Only files with the following extensions are allowed: jpg, jpeg, png.', 'Close');
+                    fileInputField.filestyle('clear');
+                    return true;
+                }
+            }
+
+            function isFileSizeTooLarge(file) {
+                // Show Info modal when user tries to upload file larger than 4MB 
+                // Similar to the source https://www.geeksforgeeks.org/validation-of-file-size-while-uploading-using-javascript-jquery/
+                const size = (file.size / 1024 / 1024).toFixed(2);
+                const maxSizeMb = 4;
+                if (size > maxSizeMb) {
+                    updateAndToggleInfoModal('File is too large', 'Maximum file size must be 4MB.', 'Close');
+                    fileInputField.filestyle('clear');
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
         }
     });
 });
